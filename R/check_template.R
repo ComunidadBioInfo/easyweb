54c36a5ac982d3d414638fdd2b6d4f0415b8c15d
Leonardo Collado Torres <lcollado@jhsph.edu>
2019-08-02 11:01:41 -0500

    Add a sample presentation pdf file, update presentations

--- R/check_template.R
+++ R/check_template.R
@@ -1,6 +1,6 @@
 check_template <- function(template) {
     ## Check that the template is valid
-    valid_templates <- c('index.Rmd', 'publications.Rmd', 'presentations.Rmd', 'contact.Rmd', '_config.yml', '_site.yml', 'avatar.jpg')
+    valid_templates <- c('index.Rmd', 'publications.Rmd', 'presentations.Rmd', 'contact.Rmd', '_config.yml', '_site.yml', 'avatar.jpg', 'sample_presentation.pdf')
 
     if(!template %in% valid_templates) {
         stop("`template` is not among the valid options. Valid options are:\n",
--- R/easyweb.R
+++ R/easyweb.R
@@ -13,10 +13,11 @@ easyweb <- function(web_tsv, path = tempdir(), publish = FALSE, overwrite = FALS
     ## Read the format
     web_tab <- read_web_tsv(web_tsv)
 
-    templates <- c('index.Rmd', '_config.yml', '_site.yml', 'presentations.Rmd', 'publications.Rmd', 'contact.Rmd', 'avatar.jpg')
+    templates <- c('index.Rmd', '_config.yml', '_site.yml', 'presentations.Rmd', 'publications.Rmd', 'contact.Rmd', 'avatar.jpg', 'sample_presentation.pdf')
     purrr::walk(templates, move_template_file, path = path, overwrite = overwrite)
 
-    ## Use web_tsv to update the template files
+    ## Update the template files
+    update_presentations(path = path, web_tab = web_tab)
     create_citations(path = path, web_tab = web_tab)
     update_publications_scholar(path = path, web_tab = web_tab)
     purrr::pwalk(
@@ -30,8 +31,7 @@ easyweb <- function(web_tsv, path = tempdir(), publish = FALSE, overwrite = FALS
         path = path
     )
 
-
-    ## TODO
+    ## Make it and rproject
 
     ## Create the website
     rmarkdown::render_site(input = path)
@@ -52,7 +52,7 @@ easyweb <- function(web_tsv, path = tempdir(), publish = FALSE, overwrite = FALS
 # dir.create('~/Desktop/test', showWarnings = FALSE)
 # web_tab <- data.frame(
 #     tag = c('name', 'github', 'linkedin', 'googlescholar', 'doi', 'doi', 'presentation', 'presentation', 'email', 'email'),
-#     value = c('Leonardo Collado-Torres', 'lcolladotor', 'linkedid_username', 'h57-MykAAAAJ', '10.1038/s41593-018-0197-y', '10.1038/nbt.3838', 'title', 'title', 'lcolladotor@gmail.com', 'leo.collado@libd.org')
+#     value = c('Leonardo Collado-Torres', 'lcolladotor', 'linkedid_username', 'h57-MykAAAAJ', '10.1038/s41593-018-0197-y', '10.1038/nbt.3838', '[CDSB2019 workshop](https://github.com/ComunidadBioInfo/cdsb2019). Taught how to make R packages (TIB2019, Cuernavaca, July 2019)', 'LIIGH 2019. recount-brain and BrainSEQ Phase II scientific talk (LIIGH seminar, Juriquilla, August 2019)', 'lcolladotor@gmail.com', 'leo.collado@libd.org')
 # )
 # write.table(web_tab, file = '~/Desktop/test/web.tsv', sep = '\t', quote = FALSE, row.names = FALSE)
 # easyweb('~/Desktop/test/web.tsv', path = '~/Desktop/test', overwrite = TRUE)
