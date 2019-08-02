416f74c988abbea858f943a6e73b7a339073e627
Claire Rioualen <rioualenclaire@gmail.com>
2019-08-02 12:35:49 -0500

    Added unit tests for file web_tsv.

--- tests/testthat/test-check_web_tsv.R
+++ tests/testthat/test-check_web_tsv.R
@@ -1,3 +1,11 @@
 test_that("Provide path to a valid input file", {
-    expect_error(check_web_tsv('not_a_valid_file'), 'File `web_tsv` does not exist')
+    expect_error(check_web_tsv("nonexisting_file.tsv"), 'File `web_tsv` does not exist')
+})
+
+test_that("Provided file has 2 columns", {
+    expect_error(check_web_tsv("3col_file.tsv"), 'File `web_tsv` is not correctly formatted')
+})
+
+test_that("Tags are valid", {
+    expect_error(check_web_tsv("notatag_file.tsv"), '`tag` is not among the valid options')
 })
